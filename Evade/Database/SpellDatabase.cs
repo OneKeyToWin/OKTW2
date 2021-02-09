022fa2e7d63be4d64affcdab701a3f7cbd7e6f9b
Dorlander <61416392+Dorlander@users.noreply.github.com>
2021-02-09 13:12:49 +0700

    Update SpellDatabase.cs

--- Evade/Database/SpellDatabase.cs
+++ Evade/Database/SpellDatabase.cs
@@ -640,7 +640,7 @@ namespace Evade
                     Slot = SpellSlot.Q,
                     Type = SkillShotType.SkillshotMissileLine,
                     Delay = 250,
-                    Range = 1150,
+                    Range = 1155,
                     Radius = 70,
                     MissileSpeed = 1800,
                     FixedRange = true,
